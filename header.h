ae5fd59f90551bb627fb48a1b687842012dab754
Eduardo-Castillo-Sfleet <eduardo.castillo@sfleet.mx>
2021-01-24 18:02:32 -0600

    header

---
+++ header.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <time.h>
+
+#define PNGFORMATO 0x5D40
+#define FRAGMENTO 64
+#define TAMANO 128
+
+void iniciar();
+void fragmentarImagen();
+void sumarImagenes();
+void multiImagenes();
+void guardarPNG();
+void tiempo();
+
+
+
+
